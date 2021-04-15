bcb0fc4a5ceb006bd79d456d548025ec06713ea7
Scott Geryol <sgeryol@gmail.com>
2021-04-14 21:08:45 -0500

    initial commit

--- if_statement.rb
+++ if_statement.rb
@@ -1,3 +1,5 @@
+puts "HELLO WORLD! IM BACK!"
+
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
